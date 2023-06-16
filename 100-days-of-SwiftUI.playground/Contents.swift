c93cad9db08492de277f316fd7f14172190468a0
Miranda Ragland <mirandajanell@gmail.com>
2023-06-15 17:19:47 -0700

    Day 1 - whole numbers lesson.

--- 100-days-of-SwiftUI.playground/Contents.swift
+++ 100-days-of-SwiftUI.playground/Contents.swift
@@ -43,3 +43,30 @@ print(result.uppercased())
 print(movie.hasPrefix("A day"))
 
 print(filename.hasSuffix(".jpg"))
+
+let score = 10
+
+let reallyBig = 100000000
+let reallyBig2 = 100_000_000
+let reallyBig3 = 1_00__00___00____00
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter += 5
+print(counter)
+counter *= 5
+print(counter)
+counter -= 5
+print(counter)
+counter /= 5
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
