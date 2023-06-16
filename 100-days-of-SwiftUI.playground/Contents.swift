a4406148038821cd27a157d17772d630d8ec0016
Miranda Ragland <mirandajanell@gmail.com>
2023-06-16 15:47:29 -0700

    Day 2 - Booleans lesson.

--- 100-days-of-SwiftUI.playground/Contents.swift
+++ 100-days-of-SwiftUI.playground/Contents.swift
@@ -1,94 +1,43 @@
-import Cocoa
-
-var greeting = "Hello, playground"
-
-var name = "Ted"
-name = "Rebecca"
-name = "Keely"
-let character = "Daphne"
-
-var playerName = "Roy"
-print(playerName)
-
-playerName = "Dani"
-print(playerName)
-
-playerName = "Sam"
-print(playerName)
-
-let managerName = "Michael Scott"
-let dogBreed = "Samoyed"
-let meaningOfLife = "How many roads must a man walk down?"
-
-let actor = "Denzel Washington"
+// Day 2 - How to store truth with Booleans
 
 let filename = "paris.jpg"
-let result = "⭐️ You win! ⭐️"
-
-let quote = "Then he tapped a sign saying \"Believe\" and walked away."
-
-let movie = """
-A day in
-the life of an
-Apple engineer
-"""
-
-print(actor.count)
-
-let nameLength = actor.count
-print(nameLength)
-
-print(result.uppercased())
-
-print(movie.hasPrefix("A day"))
-
 print(filename.hasSuffix(".jpg"))
-
-let score = 10
-
-let reallyBig = 100000000
-let reallyBig2 = 100_000_000
-let reallyBig3 = 1_00__00___00____00
-
-let lowerScore = score - 2
-let higherScore = score + 10
-let doubledScore = score * 2
-let squaredScore = score * score
-let halvedScore = score / 2
-print(score)
-
-var counter = 10
-counter += 5
-print(counter)
-counter *= 5
-print(counter)
-counter -= 5
-print(counter)
-counter /= 5
-print(counter)
 
 let number = 120
 print(number.isMultiple(of: 3))
-print(120.isMultiple(of: 3))
 
-let number2 = 0.1 + 0.2
-print(number2)
+let goodDogs = true
+let gameOver = false // damn you for making me think of the game, it truly never ends
 
-let a = 1
-let b = 2.0
-//let c = a + b // causes error, cannot add integer and double
-let c1 = a + Int(b)
-let c2 = Double(a) + b
+let isMultiple = 120.isMultiple(of: 3)
 
-let double1 = 3.1
-let double2 = 3131.3131
-let double3 = 3.0
-let int1 = 3
+var isAuthenticated = false;
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
 
-var name2 = " Nicolas Cage"
-name2 = "John Travolta"
-// name2 = 57 // causes error, type safety
+var gameOver1 = false
+print(gameOver1)
+gameOver1.toggle()
+print(gameOver1)
 
-var rating = 5.0
-rating *= 2
-
+// do we get boolean operators?
+// not part of the lesson, just my curiousity
+let andTestTrue = goodDogs && gameOver1
+print(andTestTrue)
+let andTestFalse = goodDogs && gameOver
+print(andTestFalse)
+let orTestTrue = goodDogs || gameOver
+print(orTestTrue)
+let orTestFalse = gameOver || isAuthenticated
+print(orTestFalse)
+//let xorTestTrue1 = goodDogs ^^ gameOver // cannot find operator
+let eqTrue = goodDogs == gameOver1
+print(eqTrue)
+let eqFalse = goodDogs == gameOver
+print(eqFalse)
+let neqTrue = goodDogs != gameOver
+print(neqTrue)
+let neqFalse = goodDogs != gameOver1
+print(neqFalse)
