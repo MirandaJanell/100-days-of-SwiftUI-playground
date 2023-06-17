7a15eb37169130d54a3aa281c263f63db3729d2b
Miranda Ragland <mirandajanell@gmail.com>
2023-06-17 16:38:09 -0700

    Day 3 - How to store and find data in dictionaries lesson.

--- 100-days-of-SwiftUI.playground/Contents.swift
+++ 100-days-of-SwiftUI.playground/Contents.swift
@@ -1,55 +1,58 @@
-// Day 3 - How to store ordered data in arrays
+// Day 3 - How to store and find data in dictionaries
 
-var beatles = ["John", "Paul", "George", "Ringo"]
-let numbers = [4, 8, 15, 16, 23, 42]
-var temperatures = [25.3, 28.2, 26.4]
-print(beatles[0])
-print(numbers[1])
-print(temperatures[2])
+var employee = ["Taylor Swift", "Singer", "Nashville"]
+print("Name: \(employee[0])")
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
 
-beatles.append("Adrian")
-beatles.append("Allen")
-beatles.append("Adrian")
-beatles.append("Novall")
-beatles.append("Vivian")
+print("Name: \(employee[0])")
+employee.remove(at: 1)
+print("Job title: \(employee[1])")
+// bounds error
+//print("Location: \(employee[2])")
 
-// not allowed, violates type safety
-//temperatures.append("Chris")
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+print(employee2["name"])
+print(employee2["job"])
+print(employee2["location"])
+print(employee2["password"])
+print(employee2["status"])
+print(employee2["manager"])
 
-var scores = Array<Int>()
-scores.append(100)
-scores.append(80)
-scores.append(85)
-print(scores[1])
+print(employee2["name", default: "Unknown"])
+print(employee2["job", default: "Unknown"])
+print(employee2["location", default: "Unknown"])
+print(employee2["password", default: "Unknown"])
+print(employee2["status", default: "Unknown"])
+print(employee2["manager", default: "Unknown"])
 
-var albums = Array<String>()
-albums.append("Folklore")
-albums.append("Fearless")
-albums.append("Red")
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false,
+]
 
-var albums2 = [String]()
-albums2.append("Folklore")
-albums2.append("Fearless")
-albums2.append("Red")
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo"
+]
+print(olympics[2012, default: "Unknown"])
 
-print(albums2.count)
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
 
-var characters = ["Lana", "Pam", "Ray", "Sterling"]
-print(characters.count)
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+archEnemies["Batman"] = "Penguin"
 
-characters.remove(at: 2)
-print(characters.count)
-
-characters.removeAll()
-print(characters.count)
-
-let bondMovies = ["Casino Royale", "Spectre", "No Time To Die"]
-print(bondMovies.contains("Frozen"))
-
-let cities = ["London", "Tokyo", "Rome", "Budapest"]
-print(cities.sorted())
-
-let presidents = ["Bush", "Obama", "Jackass", "Biden"]
-print(presidents)
-let reversedPresidents = presidents.reversed();
-print(reversedPresidents)
+print(archEnemies.count)
+archEnemies.removeAll();
+print(archEnemies.count)
