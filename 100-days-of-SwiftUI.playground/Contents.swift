ec80dbfdb5ed62ed70e8f23bf2e53db6c29a1da5
Miranda Ragland <mirandajanell@gmail.com>
2023-06-17 16:47:03 -0700

    Day 3 - How to use sets for fast data lookup lesson.

--- 100-days-of-SwiftUI.playground/Contents.swift
+++ 100-days-of-SwiftUI.playground/Contents.swift
@@ -1,58 +1,12 @@
-// Day 3 - How to store and find data in dictionaries
+// Day 3 - How to use sets for fast data lookup
 
-var employee = ["Taylor Swift", "Singer", "Nashville"]
-print("Name: \(employee[0])")
-print("Job title: \(employee[1])")
-print("Location: \(employee[2])")
+let people = Set(["Denzel Washington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
+print(people)
 
-print("Name: \(employee[0])")
-employee.remove(at: 1)
-print("Job title: \(employee[1])")
-// bounds error
-//print("Location: \(employee[2])")
-
-let employee2 = [
-    "name": "Taylor Swift",
-    "job": "Singer",
-    "location": "Nashville"
-]
-print(employee2["name"])
-print(employee2["job"])
-print(employee2["location"])
-print(employee2["password"])
-print(employee2["status"])
-print(employee2["manager"])
-
-print(employee2["name", default: "Unknown"])
-print(employee2["job", default: "Unknown"])
-print(employee2["location", default: "Unknown"])
-print(employee2["password", default: "Unknown"])
-print(employee2["status", default: "Unknown"])
-print(employee2["manager", default: "Unknown"])
-
-let hasGraduated = [
-    "Eric": false,
-    "Maeve": true,
-    "Otis": false,
-]
-
-let olympics = [
-    2012: "London",
-    2016: "Rio de Janeiro",
-    2021: "Tokyo"
-]
-print(olympics[2012, default: "Unknown"])
-
-var heights = [String: Int]()
-heights["Yao Ming"] = 229
-heights["Shaquille O'Neal"] = 216
-heights["LeBron James"] = 206
-
-var archEnemies = [String: String]()
-archEnemies["Batman"] = "The Joker"
-archEnemies["Superman"] = "Lex Luthor"
-archEnemies["Batman"] = "Penguin"
-
-print(archEnemies.count)
-archEnemies.removeAll();
-print(archEnemies.count)
+var people2 = Set<String>()
+people2.insert("Denzel Washington")
+people2.insert("Tom Cruise")
+people2.insert("Nicolas Cage")
+people2.insert("Samuel L Jackson")
+print(people2.count)
+print(people2.sorted())
