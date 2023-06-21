c9225135fe3fa0b8d8ffd009a8f6d073fd0199b9
Miranda Ragland <mirandajanell@gmail.com>
2023-06-20 17:50:04 -0700

    Day 5 - How to use the ternary conditional operator for quick tests lesson.

--- 100-days-of-SwiftUI.playground/Contents.swift
+++ 100-days-of-SwiftUI.playground/Contents.swift
@@ -1,80 +1,35 @@
-// Day 5 - How to use switch statements to check multiple conditions
+// Day 5 - How to use the ternary conditional operator for quick tests
 
-enum Weather {
-    case sun, rain, wind, snow, unknown
+let age = 18
+let canVote = age >= 18 ? "Yes" : "No"
+
+let hour = 23
+print(hour < 12 ? "It's before noon" : "It's after noon")
+
+let names = ["Jayne", "Kaylee", "Mal"]
+let crewCount = names.isEmpty ? "No one" : "\(names.count) people"
+print(crewCount)
+
+enum Theme {
+    case light, dark
 }
 
-let forecast = Weather.sun
+let theme = Theme.dark
 
-if forecast == .sun {
-    print("It should be a nice day.")
-} else if forecast == .rain {
-    print("Pack an umbrella.")
-} else if forecast == .wind {
-    print("Wear something warm")
-} else if forecast == .rain {
-    print("School is cancelled.")
+let background = theme == .dark ? "black" : "white"
+print(background)
+
+
+print(hour < 12 ? "It's before noon" : "It's after noon")
+/*print( // Invalid code
+    if hour < 12 {
+        "It's before noon"
+    } else {
+        "It's after noon"
+    }
+)*/
+if hour < 12 {
+    print("It's before noon")
 } else {
-    print("Our forecast generator is broken!")
-}
-
-switch forecast {
-case .sun:
-    print("It should be a nice day.")
-case .rain:
-    print("Pack an umbrella.")
-case .wind:
-    print("Wear something warm")
-case .snow:
-    print("School is cancelled.")
-case .unknown:
-    print("Our forecast generator is broken!")
-}
-
-let place = "Metropolis"
-
-switch place {
-case "Gotham":
-    print("You're Batman!")
-case "Mega-City One":
-    print("You're Judge Dredd!")
-case "Wakanda":
-    print("You're Black Panther!")
-default:
-    print("Who are you?")
-}
-
-let day = 5
-print("My true love gave to me…")
-
-switch day {
-case 5:
-    print("5 golden rings")
-case 4:
-    print("4 calling birds")
-case 3:
-    print("3 French hens")
-case 2:
-    print("2 turtle doves")
-default:
-    print("A partridge in a pear tree")
-}
-
-print("My true love gave to me…")
-
-switch day {
-case 5:
-    print("5 golden rings")
-    fallthrough
-case 4:
-    print("4 calling birds")
-    fallthrough
-case 3:
-    print("3 French hens")
-    fallthrough
-case 2:
-    print("2 turtle doves")
-    fallthrough
-default:
-    print("A partridge in a pear tree")
+    print("It's after noon")
 }
