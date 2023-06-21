066f53ecf9eedd8cbbf4a7ec80e91e4f2dceba6f
Miranda Ragland <mirandajanell@gmail.com>
2023-06-20 17:40:36 -0700

    Day 5 - How to check multiple conditions lesson.

--- 100-days-of-SwiftUI.playground/Contents.swift
+++ 100-days-of-SwiftUI.playground/Contents.swift
@@ -1,85 +1,67 @@
-// Day 5 - How to check a condition is true or false
+// Day 5 - How to check multiple conditions
 
-let score = 85
+let age = 16
 
-if score > 80 {
-    print("Great job!")
+if age >= 18 {
+    print("You can vote in the next election.")
 }
 
-let speed = 88
-let percentage = 85
-let age = 18
-
-if speed <= 88 {
-    print("Where we're going we don't need roads.") // we'll get to see some serious 💩
-}
-
-if percentage < 85 {
-    print("Sorry, you failed the test.")
+if age < 18 {
+    print("Sorry, you're too young to vote.")
 }
 
 if age >= 18 {
-    print("You're eligible to vote")
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
 }
 
-let ourName = "Dave Lister"
-let friendName = "Arnold Rimmer"
+let a = false
+let b = true
 
-if ourName < friendName {
-    print("It's \(ourName) vs \(friendName)")
+if a {
+    print("Code to run if a is true")
+} else if b {
+    print("Code to run if a is false but b is true")
+} else {
+    print("Code to run if both a and b are false")
 }
 
+let temp = 25
 
-if ourName > friendName {
-    print("It's \(friendName) vs \(ourName)")
+if temp > 20 {
+    if temp < 30 {
+        print("It's a nice day.")
+    }
 }
 
-// Make an array of 3 numbers
-var numbers = [1, 2, 3]
-
-// Add a 4th
-numbers.append(4)
-
-// If we have over 3 items
-if numbers.count > 3 {
-    // Remove the oldest number
-    numbers.remove(at: 0)
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
 }
 
-print(numbers)
+let userAge = 14
+let hasParentalConsent = true
 
-let country = "Canada"
-
-if country == "Australia" {
-    print("G'Day")
+if userAge >= 18 || hasParentalConsent == true {
+    print("You can buy the game")
 }
 
-let name = "Taylor Swift"
-
-if name != "Anonymous" {
-    print("Welcome, \(name)")
+if userAge >= 18 || hasParentalConsent {
+    print("You can buy the game")
 }
 
-// create the username variable
-var username = "taylorswift13"
-
-// if `username` contains an empty string
-if username == "" { // not efficient
-    // Make it equal to "Anonymous"
-    username = "Anonymous"
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
 }
 
-// Now print a welcome message
-print("Welcome, \(username)")
+let transport = TransportOption.airplane
 
-if username.count == 0 { // not efficient with large strings
-    username = "Anonymous"
-}
-
-if username.isEmpty == true { // better
-    username = "Anonymous"
-}
-
-if username.isEmpty { // best
-    username = "Anonymous"
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path…")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
 }
