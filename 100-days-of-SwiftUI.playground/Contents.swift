fb41ce84d520e8f2cea5abba8bab43b3cee910be
Miranda Ragland <mirandajanell@gmail.com>
2023-07-04 15:28:27 -0700

    Day 7 - How to customize parameter labels lesson

--- 100-days-of-SwiftUI.playground/Contents.swift
+++ 100-days-of-SwiftUI.playground/Contents.swift
@@ -1,53 +1,56 @@
-// Day 7 - How to return multiple values from functions
+// Day 7 - How to customize parameter labels
+
+func rollDice(sides: Int, count: Int) -> [Int] {
+    // Start with an empty array
+    var rolls = [Int]()
+    
+    // Roll as many dice as needed
+    for _ in 1...count {
+        // Add each result to our array
+        let roll = Int.random(in: 1...sides)
+        rolls.append(roll)
+    }
+    
+    // Send back all the rolls
+    return rolls
+}
+
+let rolls = rollDice(sides: 6, count: 4)
 
 func isUppercase(string: String) -> Bool {
     string == string.uppercased()
 }
 
-func getUser() -> [String] {
-    ["Taylor", "Swift"]
+let string = "HELLO, WORLD"
+let result = isUppercase(string: string)
+
+func isUppercase2(_ string: String) -> Bool {
+    string == string.uppercased()
 }
 
-let user = getUser()
-print("Name: \(user[0]) \(user[1])")
+let result2 = isUppercase2(string)
 
-func getUser2() -> [String: String] {
-    [
-        "firstName": "Taylor",
-        "lastName": "Swift"
-    ]
+func printTimesTables(number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
 }
 
-let user2 = getUser2()
-print("Name: \(user2["firstName", default: "Anonymous"]) \(user2["lastName", default: "Anonymous"])")
+printTimesTables(number: 5)
 
-func getUser3() -> (firstName: String, lastName: String) {
-    (firstName: "Taylor", lastName: "Swift")
+// invalid
+//func printTimesTables(for: Int) {
+//    for i in 1...12 {
+//        print("\(i) x \(for) is \(i * for)")
+//    }
+//}
+//
+//printTimesTables(for: 5)
+
+func printTimesTables(for number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
 }
 
-let user3 = getUser3()
-print("Name: \(user3.firstName) \(user3.lastName)")
-
-func getUser4() -> (firstName: String, lastName: String) {
-    ("Taylor", "Swift")
-}
-
-let user4 = getUser4()
-print("Name: \(user4.firstName) \(user4.lastName)")
-
-func getUser5() -> (String, String) {
-    ("Taylor", "Swift")
-}
-
-let user5 = getUser5()
-print("Name: \(user5.0) \(user5.1)")
-
-let firstName = user3.firstName
-let lastName = user3.lastName
-print("Name: \(firstName) \(lastName)")
-
-let (firstName2, lastName2) = user3
-print("Name: \(firstName2) \(lastName2)")
-
-let (firstName3, _) = user3
-print("Name: \(firstName3)")
+printTimesTables(for: 5)
