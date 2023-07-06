b0e029478b2bf0bbe08611735fdbe196953dd3d0
Miranda Ragland <mirandajanell@gmail.com>
2023-07-05 18:52:39 -0700

    Day 8 - How to provide default values for parameters lesson.

--- 100-days-of-SwiftUI.playground/Contents.swift
+++ 100-days-of-SwiftUI.playground/Contents.swift
@@ -1,56 +1,18 @@
-// Day 7 - How to customize parameter labels
+// Day 8 - How to provide default values for parameters
 
-func rollDice(sides: Int, count: Int) -> [Int] {
-    // Start with an empty array
-    var rolls = [Int]()
-    
-    // Roll as many dice as needed
-    for _ in 1...count {
-        // Add each result to our array
-        let roll = Int.random(in: 1...sides)
-        rolls.append(roll)
-    }
-    
-    // Send back all the rolls
-    return rolls
-}
-
-let rolls = rollDice(sides: 6, count: 4)
-
-func isUppercase(string: String) -> Bool {
-    string == string.uppercased()
-}
-
-let string = "HELLO, WORLD"
-let result = isUppercase(string: string)
-
-func isUppercase2(_ string: String) -> Bool {
-    string == string.uppercased()
-}
-
-let result2 = isUppercase2(string)
-
-func printTimesTables(number: Int) {
-    for i in 1...12 {
+func printTimesTablesOrig(for number: Int, end: Int) {
+    for i in 1...end {
         print("\(i) x \(number) is \(i * number)")
     }
 }
 
-printTimesTables(number: 5)
+printTimesTablesOrig(for: 5, end: 20)
 
-// invalid
-//func printTimesTables(for: Int) {
-//    for i in 1...12 {
-//        print("\(i) x \(for) is \(i * for)")
-//    }
-//}
-//
-//printTimesTables(for: 5)
-
-func printTimesTables(for number: Int) {
-    for i in 1...12 {
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
         print("\(i) x \(number) is \(i * number)")
     }
 }
 
+printTimesTables(for: 5, end: 20)
 printTimesTables(for: 5)
