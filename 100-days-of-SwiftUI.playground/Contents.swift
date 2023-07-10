6061815d8c33fd25360cdfb9a6560d3caafb08d0
Miranda Ragland <mirandajanell@gmail.com>
2023-07-09 18:26:52 -0700

    Day 8 - How to handle errors in functions lesson.

--- 100-days-of-SwiftUI.playground/Contents.swift
+++ 100-days-of-SwiftUI.playground/Contents.swift
@@ -1,24 +1,36 @@
-// Day 8 - How to provide default values for parameters
+// Day 8 - How to handle errors in functions
 
-func printTimesTablesOrig(for number: Int, end: Int) {
-    for i in 1...end {
-        print("\(i) x \(number) is \(i * number)")
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
     }
 }
 
-printTimesTablesOrig(for: 5, end: 20)
+let string = "12345"
 
-func printTimesTables(for number: Int, end: Int = 12) {
-    for i in 1...end {
-        print("\(i) x \(number) is \(i * number)")
-    }
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error.")
 }
-
-printTimesTables(for: 5, end: 20)
-printTimesTables(for: 5)
-
-var characters = ["Lana", "Pam", "Ray", "Sterling"]
-print(characters.count)
-//characters.removeAll() // keepingCapactiy: false is default
-characters.removeAll(keepingCapacity: true)
-print(characters.count)
